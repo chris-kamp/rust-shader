014b78d4acd00a6b747cf58720f788f3c8cfc0b2
= <=>
2023-12-01 23:01:36 +1000

    duplicate image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use image; // Import the image crate
+use std::path::Path;
+use std::io::Result;
+
+fn main() -> Result<()> {
+    // Path to the input image
+    let input_path = Path::new("imgs/input.jpg");
+
+    // Open the image file
+    let img = image::open(input_path).expect("Failed to open input image");
+
+    // Path for the output image
+    let output_path = Path::new("imgs/output.jpg");
+
+    // Save the image
+    img.save(output_path).expect("Failed to save output image");
+
+    Ok(())
 }
