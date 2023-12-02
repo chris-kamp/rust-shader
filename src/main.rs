a5779f7d3ce778cbf082f388f80ccad5c969c284
= <=>
2023-12-02 17:04:50 +1000

    shader function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,15 @@ use image::{self, GenericImageView, ImageBuffer, Rgba};
 use std::path::Path;
 use std::io::Result;
 
+fn shade(pixel: Rgba<u8>) -> Rgba<u8> {
+    return Rgba([
+        255 - pixel.0[0],
+        255 - pixel.0[1],
+        255 - pixel.0[2],
+        pixel.0[3]
+    ]);
+}
+
 fn main() -> Result<()> {
     // Path to the input image
     let input_path = Path::new("imgs/input.jpg");
@@ -14,7 +23,7 @@ fn main() -> Result<()> {
     let mut output_img = ImageBuffer::new(width, height);
 
     for (x, y, pixel) in img.pixels() {
-        let new_pixel = Rgba([0, 0, pixel.0[2], 0]);
+        let new_pixel = shade(pixel);
         output_img.put_pixel(x, y, new_pixel);
     }
 
