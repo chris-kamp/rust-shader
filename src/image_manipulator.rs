cd075069b8f0fffaa99536239b3a5ca9cd2c9df3
= <=>
2023-12-03 12:04:16 +1000

    support shaders with different block sizes

--- src/image_manipulator.rs
+++ src/image_manipulator.rs
@@ -8,11 +8,21 @@ impl ImageManipulator {
     pub fn run<S: Shader + ?Sized>(img: &DynamicImage, output_path: &Path, shader: &S) {
         let (width, height) = img.dimensions();
         let mut output_img = ImageBuffer::new(width, height);
+        let block_size = shader.block_size();
 
-        for (x, y, pixel) in img.pixels() {
-            let block = vec![(x, y, pixel)];
-            for (sx, sy, spx) in shader.shade(block) {
-                output_img.put_pixel(sx, sy, spx);
+        for yblock in (0..height).step_by(block_size) {
+            for xblock in (0..width).step_by(block_size) {
+                let mut block = vec![];
+
+                for y in yblock..(std::cmp::min(yblock + block_size as u32, height)) {
+                    for x in xblock..(std::cmp::min(xblock + block_size as u32, width)) {
+                        block.push((x, y, img.get_pixel(x, y)))
+                    }
+                }
+
+                for (sx, sy, spx) in shader.shade(block) {
+                    output_img.put_pixel(sx, sy, spx);
+                }
             }
         }
 
--- src/shaders/shader.rs
+++ src/shaders/shader.rs
@@ -3,7 +3,7 @@ use image::Rgba;
 pub trait Shader {
     fn shade(&self, block: Vec<(u32, u32, Rgba<u8>)>) -> Vec<(u32, u32, Rgba<u8>)>;
 
-    fn block_size(&self) -> u32 {
+    fn block_size(&self) -> usize {
         1
     }
 }
