de8e2a20d0fde1d670333899f3012c10611e026f
= <=>
2023-12-03 14:31:27 +1000

    support more palettes

--- src/shaders/palette_shader.rs
+++ src/shaders/palette_shader.rs
@@ -1,5 +1,6 @@
 use super::shader::Shader;
 use image::Rgba;
+use std::collections::HashMap;
 
 pub struct PaletteShader;
 
@@ -8,7 +9,7 @@ impl Shader for PaletteShader {
         let mut out_block = vec![];
 
         for (x, y, pixel) in block {
-            out_block.push((x, y, self.palette_match(pixel)));
+            out_block.push((x, y, self.palette_match(pixel, self.current_palette())));
         }
 
         return out_block;
@@ -16,18 +17,50 @@ impl Shader for PaletteShader {
 }
 
 impl PaletteShader {
-    fn palette(&self) -> Vec<Rgba<u8>> {
-        vec![
-            Rgba([0xff, 0xe5, 0xec, 255]),
-            Rgba([0xff, 0xc2, 0xd1, 255]),
-            Rgba([0xff, 0xb3, 0xc6, 255]),
-            Rgba([0xff, 0x8f, 0xab, 255]),
-            Rgba([0xfb, 0x6f, 0x92, 255]),
-        ]
+    // Hardcode the palette to use here (might make it configurable later)
+    fn current_palette(&self) -> &str {
+        "space"
     }
 
-    fn palette_match(&self, src: Rgba<u8>) -> Rgba<u8> {
-        self.palette()
+    fn palettes(&self) -> HashMap<&str, Vec<Rgba<u8>>> {
+        let mut palettes: HashMap<&str, Vec<Rgba<u8>>> = HashMap::new();
+
+        palettes.insert(
+            "pink",
+            vec![
+                Rgba([0xff, 0xe5, 0xec, 255]),
+                Rgba([0xff, 0xc2, 0xd1, 255]),
+                Rgba([0xff, 0xb3, 0xc6, 255]),
+                Rgba([0xff, 0x8f, 0xab, 255]),
+                Rgba([0xfb, 0x6f, 0x92, 255]),
+            ],
+        );
+
+        palettes.insert(
+            "summer",
+            vec![
+                Rgba([0xfa, 0xf2, 0xd3, 255]),
+                Rgba([0xf4, 0xe8, 0x69, 255]),
+                Rgba([0x30, 0x85, 0xc3, 255]),
+                Rgba([0x5c, 0xd2, 0xe6, 255]),
+            ],
+        );
+
+        palettes.insert(
+            "space",
+            vec![
+                Rgba([0x0c, 0x13, 0x4f, 255]),
+                Rgba([0x1d, 0x26, 0x7d, 255]),
+                Rgba([0x5c, 0x46, 0x9c, 255]),
+                Rgba([0xd4, 0xad, 0xfc, 255]),
+            ],
+        );
+
+        palettes
+    }
+
+    fn palette_match(&self, src: Rgba<u8>, palette_key: &str) -> Rgba<u8> {
+        self.palettes()[palette_key]
             .iter()
             .min_by_key(|&pc| {
                 let dr = pc[0] as i32 - src[0] as i32;
